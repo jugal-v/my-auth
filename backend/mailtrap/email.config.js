5caa9df8af613de294e4a05028030fe7e4a71645
Jugal Vyas <jugal.vyas@chplgroup.org>
2025-04-21 18:39:46 +0530

    fix: temp

--- backend/mailtrap/email.config.js
+++ backend/mailtrap/email.config.js
@@ -1,11 +1,11 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-    service: 'SendGrid',
-    auth: {
-        user: 'apikey',
-        // pass: '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',
-    },
-});
+// const transporter = nodemailer.createTransport({
+//     service: 'SendGrid',
+//     auth: {
+//         user: 'apikey',
+//         // pass: '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',
+//     },
+// });
 
-export default transporter;
+// export default transporter;
