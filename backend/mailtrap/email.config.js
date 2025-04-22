f9becd7f061012c9ed42faeedbb5426dfd640e79
Jugal Vyas <jugal.vyas@chplgroup.org>
2025-04-22 18:39:22 +0530

    feat: integrated forgot password page and email sending functionality and removed the pass for email

--- backend/mailtrap/email.config.js
+++ backend/mailtrap/email.config.js
@@ -5,7 +5,6 @@ const transporter = nodemailer.createTransport({
     service: 'SendGrid',
     auth: {
         user: 'apikey',
-        // pass: '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',
         pass: process.env.SENDGRID_API_KEY,
     },
 });
