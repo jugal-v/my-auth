c351a357626d1d1d77952bcb514c195ba41d60f7
Jugal Vyas <vyasjugal18@gmail.com>
2025-04-23 15:48:23 +0530

    feat: deployment changes

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,35 @@
-import express from "express";
-import { connectDb } from "./db/connectDb.js";
-import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express';
+import { connectDb } from './db/connectDb.js';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path'; 
 
 const app = express();
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-app.get('/', (req, res)=> {
-    res.send('Hello World')
-})
+const __dirname = path.resolve();
 
-app.use(express.json()) //allows us to parse json from incoming payloads
-app.use(cookieParser()) //allows us to parse incoming cookies
+app.use(express.json());
+app.use(cookieParser()); 
 
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => {
-    connectDb()
-    console.log(`Server is running on port ${PORT}`);
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '/frontend/dist/index.html'));
+    });
+}
+
+app.listen(PORT, () => {
+    connectDb();
+    console.log(`Server is running on port ${PORT}`);
+});
